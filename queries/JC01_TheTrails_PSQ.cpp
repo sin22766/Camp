3054861c46cf562114699fe1e4a82c3a13c19d6d
sin22766 <promchai2sin@gmail.com>
2020-11-24 12:56:14 +0700

    Sqrt Decomposition

--- queries/JC01_TheTrails_PSQ.cpp
+++ queries/JC01_TheTrails_PSQ.cpp
@@ -2,23 +2,45 @@
 
 using namespace std;
 
-int main() {
-    int n, m, sqrN;
-    scanf("%d%d", &n, &m);
-    sqrN = (int) sqrt(n);
+int tree[200005];
+int n, m;
 
+int maxInTree(int a, int b) {
+    a += n;
+    b += n;
+    int maxN = 0;
+    while (a <= b) {
+        if (a % 2) {
+            maxN = max(maxN, tree[a++]);
+        }
+        if (!(b % 2)) {
+            maxN = max(maxN, tree[b--]);
+        }
+
+        a /= 2;
+        b /= 2;
+    }
+    return maxN;
+}
+
+void add(int k, int x) {
+    k += n;
+    tree[k] = x;
+    for (k = k / 2; k >= 1; k /= 2) {
+        tree[k] = max(tree[2 * k], tree[2 * k + 1]);
+    }
+}
+
+int main() {
+    scanf("%d%d", &n, &m);
+
+    int height[n + 5], temp;
 
     //Range Max Query
-    int height[n + 5], rmq[sqrN];
 
     for (int i = 0; i < n; ++i) {
-        scanf("%d", &height[i]);
-        if (i % sqrN == 0) {
-            rmq[i / sqrN] = height[i];
-        }
-        if (rmq[i / sqrN] < height[i]) {
-            rmq[i / sqrN] = height[i];
-        }
+        scanf("%d", &temp);
+        add(i, temp);
     }
 
     //Prefix Sum Query
@@ -32,30 +54,7 @@ int main() {
     for (int i = 0; i < m; ++i) {
         int s, t;
         scanf("%d%d", &s, &t);
-
-        long long maxAns = height[s], sumAns;
-
-        sumAns = qs[t + 1] - qs[s];
-
-        while (s < t && (s % sqrN)) {
-            if (height[s] > maxAns) {
-                maxAns = height[s];
-            }
-            ++s;
-        }
-        while (s + sqrN <= t) {
-            if (rmq[s/sqrN] > maxAns) {
-                maxAns = rmq[s/sqrN];
-            }
-            ++s;
-        }
-        while (s <= t) {
-            if (height[s] > maxAns) {
-                maxAns = height[s];
-            }
-            ++s;
-        }
-        cout << maxAns << " " << sumAns << "\n";
+        cout << maxInTree(s, t) << " " << qs[t + 1] - qs[s] << "\n";
     }
     return 0;
 }
--- queries/JC02_TheVotes.cpp
+++ queries/JC02_TheVotes.cpp
@@ -3,6 +3,7 @@
 using namespace std;
 
 int main() {
+    int n, q;
 
     return 0;
 }
